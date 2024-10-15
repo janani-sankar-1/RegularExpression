3956a87441b2182be60a2486614e8f09e2f48265
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 19:22:59 +0530

    Add project files.

---
+++ RegularExpression/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+
+namespace RegularExpression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Regex regex = new Regex("^[A-Za-z]*$");
+            Console.WriteLine("Enter a value:");
+            string inputValue=Console.ReadLine();
+            bool result=regex.IsMatch(inputValue);
+            Console.WriteLine(result);
+            if(result==true )
+            {
+                Console.WriteLine("valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            Console.ReadKey();
+            //[0-9]
+            //[a-z]
+            //[A-z]
+            //[1,2,3]
+            //\d --> 1,2,3,4,5,6,7,8,9,0
+            //\w --> a
+            //\s --> white space
+
+        }
+    }
+}
